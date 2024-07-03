9bc7de87c23f1a1d26d13f5f80fb78fde2b5a028
Sofia <sofia.cronstrom@gmail.com>
2024-07-03 13:51:13 +0200

    exercise 1-4

---
+++ script.js
@@ -0,0 +1,31 @@
+////Exercise 1
+
+console.log('Hello world');
+
+////Exercise 2
+
+let myAge = 36;
+console.log(`I am ${myAge} years old`);
+
+////Exercise 3
+
+const strType = "Im a string";
+console.log(strType);
+
+const numType = 36;
+console.log(numType);
+
+const booType = true;
+console.log(booType);
+
+////Exercise 4
+
+// const myName = "Sofia";
+// console.log(myName);
+// myName = "Johnny";
+// console.log(myName)
+
+let myName = "Sofia";
+console.log(myName);
+myName = "Johnny";
+console.log(myName)
